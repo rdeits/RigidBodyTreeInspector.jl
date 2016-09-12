5ece00a6f71cb328c6ae9f744c88de63088c2d61
Robin Deits <robin.deits@gmail.com>
2016-09-12 16:05:13 -0400

    normalize quaternions when animating

--- src/RigidBodyTreeInspector.jl
+++ src/RigidBodyTreeInspector.jl
@@ -187,6 +187,14 @@ end
 
 one(::Type{Array{Float64,1}}) = 1.
 
+normalize_configuration!(jointType::JointType, q) = nothing
+function normalize_configuration!(jointType::QuaternionFloating, q)
+    n = norm(q[1:4])
+    for i = 1:4
+        q[i] /= n
+    end
+end
+
 function animate(vis::Visualizer, mechanism::Mechanism{Float64}, times::Vector{Float64}, configurations::Vector{Vector{Float64}};
     fps::Float64 = 30., realtimerate::Float64 = 1.)
     state = MechanismState(Float64, mechanism)
@@ -194,7 +202,14 @@ function animate(vis::Visualizer, mechanism::Mechanism{Float64}, times::Vector{F
     interpolated_configurations = interpolate((times,), configurations, Gridded(Linear()))
     for t in times[1] : dt : times[end]
         tic()
-        set_configuration!(state, interpolated_configurations[t])
+        q = interpolated_configurations[t]
+        for joint in joints(mechanism)
+            q_range = mechanism.qRanges[joint]
+            q_joint = q[q_range]
+            normalize_configuration!(joint.jointType, q_joint)
+            configuration(state, joint)[:] = q_joint
+        end
+        setdirty!(state)
         draw(vis, state)
         sleep(max(dt - toq(), 0) / realtimerate)
     end
