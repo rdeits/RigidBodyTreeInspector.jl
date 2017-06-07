6bc04838bfce4a665a41d49cf09bbacbd46c2872
Twan Koolen <koolen.twan@gmail.com>
2017-06-07 15:06:01 +0300

    Improve support for mechanisms with loops.
    Draw cylinders between all joint frames fixed to a body.
    
    Draw spheres at frames before all joints (not only tree joints).

--- src/geometry.jl
+++ src/geometry.jl
@@ -81,8 +81,8 @@ function inertial_ellipsoid(mechanism::Mechanism, body::RigidBody)
     return geometry, principal_axes_com_frame
 end
 
-function create_geometry_for_translation(mechanism, body, joint, radius)
-    joint_to_joint = fixed_transform(mechanism, joint.frameBefore, default_frame(body))
+function create_frame_to_frame_geometry(mechanism, body, frame1, frame2, radius)
+    joint_to_joint = fixed_transform(mechanism, frame1, frame2)
     trans = translation(joint_to_joint)
     Rx = rotation_from_x_axis(trans)
     geom_length = norm(trans)
@@ -90,28 +90,33 @@ function create_geometry_for_translation(mechanism, body, joint, radius)
                                                Translation(geom_length / 2, 0, 0)),
                                        LinearMap(AngleAxis(pi/2, 0, 1, 0)))
     frame = CartesianFrame3D("$(RigidBodyDynamics.name(body)) joint-to-joint translation")
-    tform = Transform3D(frame, default_frame(body),
+    tform = Transform3D(frame, joint_to_joint.to,
                         joint_to_geometry_origin.m,
                         joint_to_geometry_origin.v)
     add_frame!(body, tform)
-    return HyperCylinder{3, Float64}(geom_length, radius), frame
+    HyperCylinder{3, Float64}(geom_length, radius), frame
 end
 
-function maximum_link_length{T}(mechanism::Mechanism{T})
+function maximum_link_length{T}(body_fixed_joint_frames::Dict{RigidBody{T}, Vector{CartesianFrame3D}})
     result = zero(T)
-    for joint in tree_joints(mechanism)
-        parent_default_frame = default_frame(predecessor(joint, mechanism))
-        transform = fixed_transform(mechanism, parent_default_frame, frame_before(joint))
-        result = max(result, norm(translation(transform)))
+    for (body, joint_frames) in body_fixed_joint_frames
+        for framei in joint_frames, framej in joint_frames
+            transform = fixed_transform(body, framei, framej)
+            result = max(result, norm(translation(transform)))
+        end
     end
     result
 end
 
 function create_geometry(mechanism; show_inertias::Bool=false, randomize_colors::Bool=true)
-    box_width = 0.05 * maximum_link_length(mechanism)
+    body_fixed_joint_frames = Dict(body => begin
+        [map(frame_before, out_joints(body, mechanism)); map(frame_after, in_joints(body, mechanism))]
+    end for body in bodies(mechanism))
 
+    box_width = 0.05 * maximum_link_length(body_fixed_joint_frames)
     vis_data = OrderedDict{CartesianFrame3D, Vector{GeometryData}}()
     link_names = Set()
+
     for body in bodies(mechanism)
         if randomize_colors
             color = RGBA{Float64}(rand(3)..., 0.5)
@@ -122,16 +127,16 @@ function create_geometry(mechanism; show_inertias::Bool=false, randomize_colors:
             ellipsoid, ellipsoid_frame = inertial_ellipsoid(mechanism, body)
             vis_data[ellipsoid_frame] = [GeometryData(ellipsoid, color)]
         else
-            frame = default_frame(body)
-            vis_data[frame] = [GeometryData(HyperSphere{3, Float64}(
-                zero(Point{3, Float64}), box_width), color)]
+            for joint in out_joints(body, mechanism)
+                vis_data[frame_before(joint)] = [GeometryData(HyperSphere{3, Float64}(
+                    zero(Point{3, Float64}), box_width), color)]
+            end
         end
-        if !isroot(body, mechanism)
-            for joint in joints_to_children(body, mechanism)
-                geom, geom_frame = create_geometry_for_translation(mechanism,
-                                                                   body,
-                                                                   joint,
-                                                                   box_width / 2)
+        frames = body_fixed_joint_frames[body]
+        for (i, framei) in enumerate(frames)
+            for j = i + 1 : length(frames)
+                framej = frames[j]
+                geom, geom_frame = create_frame_to_frame_geometry(mechanism, body, framei, framej, box_width / 2)
                 vis_data[geom_frame] = [GeometryData(geom, color)]
             end
         end
